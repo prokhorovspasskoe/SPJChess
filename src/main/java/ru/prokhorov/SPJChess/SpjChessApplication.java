9824d2895f4edeaf9a681fb089cff292167fbe58
Prokhorov <prokhorovspasskoe@yandex.ru>
2023-03-08 11:46:57 +0300

    Initial commit

---
+++ src/main/java/ru/prokhorov/SPJChess/SpjChessApplication.java
@@ -0,0 +1,13 @@
+package ru.prokhorov.SPJChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpjChessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prokhorov/SPJChess/SpjChessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prokhorov.SPJChess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpjChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
